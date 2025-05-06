ad8a7e96ca45ab5ac56ff42f54cd6f7b630534dc
paul <3167385363@qq.com>
2025-05-06 17:43:29 +0800

    添加vercel 分析

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "../styles/global.scss"
 import Head from "next/head"
+import { Analytics } from "@vercel/analytics/react"
 export default function App({ Component, pageProps }) {
     return (
       <>
