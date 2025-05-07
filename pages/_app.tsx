3c0421801ce3aad512bc140a73ad13337ca3de2a
paul <3167385363@qq.com>
2025-05-07 13:23:49 +0800

    迁移

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import "../styles/global.scss"
 import Head from "next/head"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function App({ Component, pageProps }) {
     return (
       <>
