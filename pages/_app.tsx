1ebe4eaa784640ce70016c0cf8c09a397d4d0c0f
paul <3167385363@qq.com>
2025-05-29 11:53:20 +0800

    seo冲击

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/global.scss"
 import Head from "next/head"
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import seo from "./seo.json"
 export default function App({ Component, pageProps }) {
     return (
       <>
@@ -10,6 +11,12 @@ export default function App({ Component, pageProps }) {
           children={
             <>
               <title>Paul's Blog</title>
+              <title>{seo.map(s=>s.title+" ")}</title>
+               {
+                seo.map((s, index) => (
+                  <meta key={index} name={s.title} content={s.content} />
+                ))
+               }
               <meta name="baidu-site-verification" content="codeva-UOeGseNARG" />
             </>
           }
