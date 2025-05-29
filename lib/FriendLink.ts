3a098ef9d7bccfc1b3a0321e29d4d61109a1961b
paul <3167385363@qq.com>
2025-05-29 15:47:14 +0800

    增加友链

--- lib/FriendLink.ts
+++ lib/FriendLink.ts
@@ -14,9 +14,14 @@ export   const links: FriendLink[] = [
   {
     text: "飞's blog",
     path: "https://zzf-gitch.github.io/vitepress-zzf/",
-  },{
+  },
+  {
     text: "Ling's blog",
     path: "https://blog.lty520.faith/",
+  },
+  {
+    text: "冬天的小窝",
+    path: "https://www.iamdt.cn/",
   },
 ];
 
